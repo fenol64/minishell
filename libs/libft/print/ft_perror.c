d7893257568e813d12f6fc867df2fb8a4a77816d
paulhenr <voxskynned@gmail.com>
2024-04-16 12:08:28 -0300

    fixed same name buffer error

--- libs/libft/print/ft_perror.c
+++ libs/libft/print/ft_perror.c
@@ -6,24 +6,24 @@
 /*   By: paulhenr <paulhenr@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 11:01:14 by paulhenr          #+#    #+#             */
-/*   Updated: 2024/04/16 12:03:50 by paulhenr         ###   ########.fr       */
+/*   Updated: 2024/04/16 12:07:40 by paulhenr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/libft.h"
 
-#define BUFFER_SIZE 5124
+#define FT_PERROR_BUFFER_SIZE 5124
 
 int	ft_perror(const char *s1, const char *s2)
 {
-	static char	buffer[BUFFER_SIZE];
+	static char	buffer[FT_PERROR_BUFFER_SIZE];
 
 	buffer[0] = '\0';
 	if (!s1 || !s2)
 		return (ft_perror(__func__, "argument is NULL"), 1);
 	if (!*s1 && !*s2)
 		return (ft_perror(__func__, "all arguments are empty"), 1);
-	if (ft_strlen(s1) + ft_strlen(s2) >= BUFFER_SIZE)
+	if (ft_strlen(s1) + ft_strlen(s2) >= FT_PERROR_BUFFER_SIZE)
 		return (ft_perror(__func__, "error msg is too big"), 1);
 	if (*s1)
 		ft_strcat(buffer, s1);
