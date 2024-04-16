c5d6f0c9905dbb0a20f52ad8ac47d784e37412e1
paulhenr <voxskynned@gmail.com>
2024-04-16 13:04:11 -0300

    aassadahsdahkhsfdkshsd

--- src/mini_includes/free_structs.c
+++ src/mini_includes/free_structs.c
@@ -6,7 +6,7 @@
 /*   By: paulhenr <paulhenr@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 10:20:31 by paulhenr          #+#    #+#             */
-/*   Updated: 2024/04/16 12:56:34 by paulhenr         ###   ########.fr       */
+/*   Updated: 2024/04/16 13:04:00 by paulhenr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,8 @@ void	free_main(t_main *main)
 		close(main->def_stdin);
 	if (main->procs)
 		free_proc_list(main->procs, free);
+	if (main->input_list)
+		lst_destroy2(main->input_list, free_token_node);
 	if (main->envp)
 		ft_free_matrix(main->envp);
 	free(main);
